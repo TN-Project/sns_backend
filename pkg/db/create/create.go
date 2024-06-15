64ee48b1cdad452b4009fe3c932073c2f1682269
natyosu3 <nachi334825@gmail.com>
2024-06-15 21:14:12 +0900

    カラムの型をintからbigintに変更 user作成機能の実装(DB)

--- pkg/db/create/create.go
+++ pkg/db/create/create.go
@@ -6,7 +6,7 @@ import (
 )
 
 var sql_stm []string = []string{
-	`create table IF NOT EXISTS "user" (user_id int PRIMARY KEY, nickname text NOT NULL, username text UNIQUE, password text)`,
+	`create table IF NOT EXISTS "user" (user_id bigint PRIMARY KEY, nickname text NOT NULL, username text UNIQUE, password text)`,
 	`create table IF NOT EXISTS "group" (group_id int PRIMARY KEY, group_name text)`,
 	`create table IF NOT EXISTS "user_group" (user_id int, group_id int, PRIMARY KEY(user_id, group_id), FOREIGN KEY(user_id) REFERENCES "user"(user_id), FOREIGN KEY(group_id) REFERENCES "group"(group_id))`,
 }
@@ -23,4 +23,18 @@ func CreateDefaultTable() {
 		}
 	}
 	slog.Info("Tables created successfully")
+}
+
+func CreateUser(user_id int, nickname string, username string, hashed_password string) error {
+	db := db.Connect()
+	defer db.Close()
+
+	sql := `insert into "user" (user_id, nickname, username, password) values ($1, $2, $3, $4)`
+	_, err := db.Exec(sql, user_id, nickname, username, hashed_password)
+	if err != nil {
+		slog.Error("Error creating user: ", err)
+		return err
+	}
+	slog.Info("User created successfully")
+	return nil
 }
