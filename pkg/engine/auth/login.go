0558554376a854f01013e7a736e68515fe2cb3c0
natyosu3 <nachi334825@gmail.com>
2024-06-17 14:24:22 +0900

    ログイン処理の実装

--- pkg/engine/auth/login.go
+++ pkg/engine/auth/login.go
@@ -1,9 +1,13 @@
 package auth
 
 import (
+	"sns_backend/pkg/common/encrypt"
+	"sns_backend/pkg/common/model"
+	"sns_backend/pkg/db/read"
+	"sns_backend/pkg/session"
+
 	"github.com/gin-gonic/gin"
 )
-
 
 type LoginRequest struct {
 	Username string `json:"username"`
@@ -21,15 +25,36 @@ func loginPost(c *gin.Context) {
 	}
 
 	// データベースからユーザ情報を取得
-	
+	user, err := read.GetUser(req.Username)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": "internal server error",
+			"error":   err.Error(),
+		})
+		return
+	}
+
 	// パスワードを検証
+	if err := encrypt.CompareHashAndPassword(user.Password, req.Password); err != nil {
+		c.JSON(400, gin.H{
+			"message": "invalid password",
+			"error":   err.Error(),
+		})
+		return
+	}
 
 	// セッション情報を設定
+	session_data := model.Session{
+		Userid:   user.User_id,
+		Nickname: user.Nickname,
+		Username: user.Username,
+	}
 
 	// セッションを設定(cookieにセット)
+	session.Default(c, "session", &model.Session{}).Set(c, session_data)
 
 	c.JSON(200, gin.H{
-		"error": nil,
+		"error":   nil,
 		"message": "login success",
 	})
 }
