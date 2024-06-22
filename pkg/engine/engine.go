a290c319e303953e7ee060bf141fbf36a33bbd10
natyosu3 <nachi334825@gmail.com>
2024-06-23 00:45:06 +0900

    chore: Update CORS headers to allow "Cookie" and "Set-Cookie" headers

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -15,7 +15,7 @@ func Engine(r *gin.Engine) *gin.Engine {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Cookie", "Set-Cookie"},
 		AllowCredentials: true,
 	}))
 
