46583adb61fbbc5a7670c3ec2e6583a74403fc31
natyosu3 <nachi334825@gmail.com>
2024-06-23 00:39:06 +0900

    一時的にCORSを削除

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -5,19 +5,18 @@ import (
 	"sns_backend/pkg/engine/group"
 	"sns_backend/pkg/engine/picture"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Engine(r *gin.Engine) *gin.Engine {
 	r.Use(gin.Logger())
 
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
-		AllowCredentials: true,
-	}))
+	// r.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"http://localhost:3000"},
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type"},
+	// 	AllowCredentials: true,
+	// }))
 
 	authGroup := r.Group("/auth")
 	{
